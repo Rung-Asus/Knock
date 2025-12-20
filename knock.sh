7f3f7b586816c05b96e1d466a023032a5814492d
Rung-Asus <176239741+Rung-Asus@users.noreply.github.com>
2025-12-20 09:15:03 -0600

    Version 1.1 Updates

--- knock.sh
+++ knock.sh
@@ -2,7 +2,7 @@
 #
 #Knock.sh: Router Commands for non-admin users
 #
-#To Install:
+#To install:
 #        1) Move script to /jffs/scripts/ directory
 #        2) Run 'sh /jffs/scripts/knock.sh -install'
 #        3) Update knock.cfg configuration file in the /jffs/addons/knock.d/ folder
@@ -10,7 +10,7 @@
 #                Port Number <space> Interface(s) [comma separated] <space> Command to execute [to end of line]
 #                Default configuration file has use-case examples:
 #                        Wake PC, reboot router, and run custom enable/disable scripts (e.g. for VPN Director rules)
-#        3) Run '/jffs/scripts/knock.sh -start'
+#        4) Run '/jffs/scripts/knock.sh -start'
 #
 #Users can now execute commands by sending port knocks
 #        (e.g. for main lan interface command, enter browser url: http://192.168.50.1:44444)
@@ -20,16 +20,21 @@
 #        Update /jffs/addons/knock.d/knock.cfg
 #        Run '/jffs/scripts/knock.sh -start'
 #
-#To Uninstall:
-#        Run '/jffs/scripts/knock.sh -uninstall'
+#To display current configuration file information:
+#        Run '/jffs/scripts/knock.sh -config'
 #
+#To update to the lastest version of script:
+#        Run '/jffs/scripts/knock.sh -update'
+#
+#To uninstall:
+#        Run '/jffs/scripts/knock.sh -uninstall'
 #
 #Many thanks to @Viktor Jaep for all his help, input and testing of this script!
 #Concepts in this script were derved from @Viktor Jaep's awesome Tailmon script
 #Original concept credit to @RMerlin (https://www.snbforums.com/threads/wake-on-lan-per-http-https-script.7958/post-47811)
-# Last Updated: 18DEC2025
+# Last Updated: 20DEC2025
 
-REV="1.0b"
+REV="1.1"
 INTERVAL=5
 
 fn=$(readlink -f "$0")
@@ -43,6 +48,29 @@ pm=$js"/post-mount"
 fs=$js"/firewall-start"
 giturl="https://raw.githubusercontent.com/Rung-Asus/Knock/main"
 
+function showconfig {
+	echo -e "The following ports/interfaces will execute these router commands:\n"
+	lastcomment=""
+	commandnum=0
+	while read port interfaces cmd
+	do
+		if [ -n "$port" ]; then
+			if [ $(echo $port | cut -c 1-1) != "#" ]; then
+				commandnum=$(($commandnum+1))
+				echo "Command #" $commandnum
+				echo -e "\t"$lastcomment
+				echo -e "\tPort" $port "on" $interfaces
+				echo -e "\tCommand:" "$cmd"
+				interface=$(echo $interfaces | awk -F',' '{print $1}')
+				echo -e "\tURL to initiate command:" $(ifconfig  $interface | awk '{print $2}' | grep addr | sed 's/addr:/http:\/\//g')":"$port
+				echo -e ""
+			else
+				lastcomment=$(echo "$port $interfaces $cmd" | cut -c 2-)
+			fi
+		fi
+	done < $cf
+}
+
 if [ "$1" = "-version" ]; then
 	echo "Version" $REV
 	exit
@@ -51,11 +79,11 @@ fi
 if [ "$1" = "-screen" ]; then
 	if [ ! -f "/opt/sbin/screen" ]; then
 		logger -t "knock.sh" "Error: Entware Screen app not installed"
-		exit
+		exit 1
 	fi
 	if [ ! -f $sf ]; then
 		logger -t "knock.sh" "Error: knock.sh not installed yet"
-		exit
+		exit 1
 	fi
 
 	logger -t "knock.sh" "Starting knock.sh background process"
@@ -68,7 +96,7 @@ fi
 if [ "$1" = "-firewall" ]; then
 	if [ ! -f $cf ]; then
 		logger -t "knock.sh" "Error: Missing configuration file" $cf
-		exit
+		exit 1
 	fi
 
 	logger -t "knock.sh" "Adding knock ports to iptables"
@@ -89,25 +117,25 @@ fi
 if [ "$1" = "-install" ]; then
 	if [ "$fn" != "$sf" ]; then
 		echo "Error: This script must be run from" $js
-		exit
+		exit 1
 	fi
 	chmod 755 $sf
 
 	if [ ! -f "/opt/sbin/screen" ]; then
 		echo "Please install Entware Screep app first"
 		echo "Run this command: 'opkg install screen'"
-		exit
+		exit 1
 	fi
 
 	if [ ! -d $jf"/addons" ]; then
 		echo "Error: This script is designed for Asuswrt-Merlin firmware only"
-		exit
+		exit 1
 	fi
 	mkdir $id 2>/dev/null
 
 	if [ ! -f $cf ]; then
 		cat <<EOF > $cf
-#knock.sh example configuration file
+#knock.sh Example configuration file
 
 #Format Port Number <space> Interface(s) [comma separated] <space> Command to execute [to end of line]
 
@@ -117,11 +145,12 @@ if [ "$1" = "-install" ]; then
 #Reboot router if 44445 knock comes from main lan, tailscale (lo), or wireguard server (wgs1)
 44445 br0,lo,wgs1 reboot
 
-#enable script example
-44446 br0 /jffs/scripts/enable-example.sh
+#Turn on VPN client
+44446 br0 /jffs/scripts/enable-wireguard-rule.sh
 
-#disable script example
-44447 br0 /jffs/scripts/disable-example.sh
+#Turn off vpn client
+44447 br0 /jffs/scripts/disable-wireguard-rule.sh
+
 
 EOF
 	fi
@@ -143,7 +172,7 @@ EOF
 	echo $sf "-firewall # Added by knock.sh" >> $fs
 
 	clear
-	echo "Knock.sh installed"
+	echo "Knock.sh Rev" $REV "installed"
 	echo ""
 	echo "Please update the configuration file" $cf
 	echo ""
@@ -157,25 +186,27 @@ fi
 if [ "$1" = "-start" ] || [ "$1" = "-restart" ]; then
 	if [ ! -f $cf ]; then
 		echo "Error: Missing configuration file" $cf
-		exit
+		exit 1
 	fi
 
 	service restart_firewall >/dev/null
 
-	$sf "-screen"
+	if $sf "-screen"; then
+		clear
+		echo "Knock.sh Rev" $REV "started and ready for port knocks"
+		echo ""
+		showconfig
+		exit
+	fi
+fi
 
+if [ "$1" = "-config" ]; then
+	if [ ! -f $cf ]; then
+		echo "Error: Missing configuration file" $cf
+		exit 1
+	fi
 	clear
-	echo "Knock.sh started and ready for port knocks"
-	echo ""
-	echo "The following ports/interfaces will execute these router commands:"
-
-	while read port interfaces cmd
-	do
-		if [ -n "$port" ] && [ $(echo $port | cut -c 1-1) != "#" ]; then
-			echo "	Port" $port "on" $interfaces "Command:" "$cmd"
-		fi
-	done < $cf
-
+	showconfig
 	exit
 fi
 
@@ -220,7 +251,6 @@ if [ "$1" = "-update" ]; then
 		done }
 
 	rm $vf 2> /dev/null
-	#curl --silent --retry 3 --connect-timeout 3 --max-time 6 --retry-delay 1 --retry-all-errors --fail "https://raw.githubusercontent.com/Rung-Asus/Knock/refs/heads/main/version.txt" -o $vf
 	curl --silent --retry 3 --connect-timeout 3 --max-time 6 --retry-delay 1 --retry-all-errors --fail $giturl"/version.txt" -o $vf
 	if [ -f $vf ]; then
 		nv=$(cat $vf | head -n 1)
@@ -228,7 +258,6 @@ if [ "$1" = "-update" ]; then
 		echo "Current version:" $REV
 		if  promptyn "Proceed with update? (y/n):" ; then
 			echo -e "\nDownloading..."
-			#curl --silent --retry 3 --connect-timeout 3 --max-time 6 --retry-delay 1 --retry-all-errors --fail "https://raw.githubusercontent.com/Rung-Asus/Knock/refs/heads/main/knock.sh" -o $sf
 			curl --silent --retry 3 --connect-timeout 3 --max-time 6 --retry-delay 1 --retry-all-errors --fail $giturl"/knock.sh" -o $sf
 			chmod 755 $sf
 			echo "Installing..."
@@ -238,21 +267,23 @@ if [ "$1" = "-update" ]; then
 			echo "Update completed."
 			echo "Installed version is now:"
 			$sf -version
+			echo ""
+			showconfig
 		else
 			echo -e "\nNo update performed"
 		fi
 	else
 		echo "Error: network issue"
+		exit 1
 	fi
 	exit
 fi
 
-
 if [ "$1" != "-loop" ]; then
 	echo "Knock.sh: Router Commands for non-admin users"
-	echo "Revision" $REV
+	echo "Version" $REV
 	echo ""
-	echo "To Install:"
+	echo "To install:"
 	echo -e "\t1) Move script to" $js"/ directory"
 	echo "	2) Run 'sh" $sf "-install'"
 	echo "	3) Update knock.cfg configuration file in the" $id"/ folder"
@@ -260,7 +291,7 @@ if [ "$1" != "-loop" ]; then
         echo "		Port Number <space> Interface(s) [comma separated] <space> Command to execute [to end of line]"
 	echo -e "\t\tDefault configuration file has use-case examples:"
 	echo -e "\t\t\tWake PC, reboot router, and run custom enable/disable scripts (e.g. for VPN Director rules)"
-	echo "	3) Run '"$sf "-start'"
+	echo "	4) Run '"$sf "-start'"
 	echo ""
 	echo "Users can now execute commands by sending port knocks"
 	echo "	(e.g. for main lan interface command, enter browser url: http://192.168.50.1:44444)"
@@ -270,7 +301,13 @@ if [ "$1" != "-loop" ]; then
 	echo "	Update" $cf
 	echo "	Run '"$sf "-start'"
 	echo ""
-	echo "To Uninstall:"
+	echo "To display current configuration file information:"
+	echo "	Run '"$sf "-config'"
+	echo ""
+	echo "To update to the lastest version of script:"
+	echo "	Run '"$sf "-update'"
+	echo ""
+	echo "To uninstall:"
 	echo "	Run '"$sf "-uninstall'"
 	echo ""
 	exit
@@ -279,7 +316,7 @@ fi
 if [ ! -f $cf ]; then
 	echo "Error: Missing configuration file" $cf
 	logger -t "knock.sh" "Error: Missing configuration file" $cf
-	exit
+	exit 1
 fi
 
 function readDATA {
@@ -290,11 +327,11 @@ function readID {
 DATA=$(readDATA)
 oldID=$(readID)
 
-
 echo "Knock.sh started"
 echo "Version" $REV
 echo "Waiting for port knocks..."
 logger -t "knock.sh" "Waiting for port knocks..."
+
 while sleep $INTERVAL;do
 	DATA=$(readDATA)
 	ID=$(readID)
